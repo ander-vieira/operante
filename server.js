e6d68c12b2344617f4dbf7941ae211e0f7962128
Ander Vieira <ander@ideable.net>
2018-01-18 13:48:33 +0100

    Valores por defecto para Handlebars (ej. titulo)

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ var express = require("express");
 var minify = require("express-minify");
 var exphbs  = require('express-handlebars');
 var favicon = require('serve-favicon');
+var defaults = require('./defaults.json');
 
 require('./logger.js');
 
 var app = express();
 app.use(favicon('./public/media/favicon.ico'));
+app.locals = Object.assign(defaults, app.locals);
 app.engine('html', exphbs({
   defaultLayout: 'main',
   extname: '.html',
   partialsDir: "views/"
- }));
+}));
 app.set('view engine', 'html');
 app.use(minify());
 app.use(express.static('./public'));
